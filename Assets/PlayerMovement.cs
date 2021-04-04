2e265d22623877d278e307c752d51bef80565819
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-04-04 18:50:12 -0400

    Begun setting up third person camera

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public Transform playerCam;
+
+    // Variables
+    private float leftRightInput, forwardBackwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftRightInput = Input.GetAxisRaw("Horizontal");
+        forwardBackwardInput = Input.GetAxisRaw("Vertical");
+
+        controller.Move(new Vector3(leftRightInput, 0, forwardBackwardInput) * 5 * Time.deltaTime);
+    }
+
+}
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+    public Transform lookAt;
+    public Transform camTranform;
+
+    public Camera cam;
+
+    private float distance = 10f;
+    //private float height;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float sensitivityX = 4f;
+    private float sensititvyY = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTranform = transform;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(45, 0, 0);
+        Vector3 pos = lookAt.position + rotation * dir;
+        pos.z = Mathf.Max(pos.z, -20);
+        camTranform.position = pos;
+        camTranform.LookAt(lookAt.position);
+    }
+}
