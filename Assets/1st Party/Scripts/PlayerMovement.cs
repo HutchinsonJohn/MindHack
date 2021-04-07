560ad6045a65bad462a6e176cc7eb5ddcbeecb14
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-04-07 04:41:10 -0400

    Update Unity version

--- Assets/1st Party/Scripts/PlayerMovement.cs
+++ Assets/1st Party/Scripts/PlayerMovement.cs
@@ -14,7 +14,10 @@ public class PlayerMovement : MonoBehaviour
 
     // Variables
     private float leftRightInput, forwardBackwardInput;
-    private float distance = 7f;
+    private float distance = 7f; //camera distance from player, more accurately its height times sin(cameraAngle)
+    private float moveSpeed = 5f;
+    private float cameraAngle = 45f;
+    private float roomBottomLimit = -19.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -37,30 +40,24 @@ public class PlayerMovement : MonoBehaviour
     {
         leftRightInput = Input.GetAxisRaw("Horizontal");
         forwardBackwardInput = Input.GetAxisRaw("Vertical");
-        controller.Move(new Vector3(leftRightInput, 0, forwardBackwardInput).normalized * 5 * Time.deltaTime);
+        controller.Move(new Vector3(leftRightInput, 0, forwardBackwardInput).normalized * moveSpeed * Time.deltaTime);
     }
 
     private void Look()
     {
-        //Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-        //mousePos.y = 0;
-        //Debug.DrawLine(transform.position, mousePos + Vector3.up * transform.position.y, Color.red);
-        //transform.LookAt(mousePos + Vector3.up * transform.position.y);
-
         Ray mousePos = viewCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        Physics.Raycast(mousePos, out hit, lookPlane);
+        Physics.Raycast(mousePos, out RaycastHit hit, lookPlane);
         Vector3 lookAt = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-        Debug.DrawLine(transform.position, lookAt, Color.red);
+        //Debug.DrawLine(transform.position, lookAt, Color.red);
         transform.LookAt(lookAt);
     }
 
     private void ThirdPersonCamera()
     {
         Vector3 dir = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(45, 0, 0);
+        Quaternion rotation = Quaternion.Euler(cameraAngle, 0, 0);
         Vector3 pos = transform.position + rotation * dir;
-        pos.z = Mathf.Max(pos.z, -19.5f);
+        pos.z = Mathf.Max(pos.z, roomBottomLimit);
         camTransform.position = pos;
         camTransform.LookAt(transform);
     }
