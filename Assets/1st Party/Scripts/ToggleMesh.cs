7562521348a1decf3a2a16559ffc507daf52a98e
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-05-06 14:34:08 -0400

    moved assets, added MH device specific script

---
+++ Assets/1st Party/Scripts/ToggleMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMesh : MonoBehaviour
+{
+
+    public GameObject box;
+    private MeshRenderer mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = box.GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mesh.enabled = false;
+        }
+    }
+}
