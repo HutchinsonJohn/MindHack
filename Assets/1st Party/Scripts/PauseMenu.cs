98a53e35cb36390f9ed4116a69b7321cffb5c3b3
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-05-09 02:09:59 -0400

    added retry level to pause menu

--- Assets/1st Party/Scripts/PauseMenu.cs
+++ Assets/1st Party/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -43,6 +44,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel"));
     }
 
     public void QuitGame()
