b4143b1c0378b076b0a323d0e98791e66b34437c
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-05-09 01:59:53 -0400

    added new game and load game to main menu, level one fix

--- Assets/1st Party/Scripts/MainMenu.cs
+++ Assets/1st Party/Scripts/MainMenu.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame()
+    public Button loadGameButton;
+    public TMP_Text loadGameText;
+
+    private void Start()
     {
+        if (PlayerPrefs.GetInt("CurrentLevel") == 0)
+        {
+            loadGameButton.interactable = false;
+            loadGameText.alpha = .25f;
+        }
+    }
+
+    
+
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt("KilledEnemies", 0);
+        PlayerPrefs.SetInt("SleptEnemies", 0);
+        PlayerPrefs.SetInt("HackedEnemies", 0);
+        PlayerPrefs.SetInt("CurrentLevel", 1);
+        PlayerPrefs.Save();
+
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel"));
     }
 
     public void QuitGame()
