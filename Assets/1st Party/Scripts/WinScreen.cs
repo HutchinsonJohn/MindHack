cbec3524dca8c346ad0f3ad4ac0853fc563054dc
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-05-10 15:34:30 -0400

    level 6 work, adjustsments to player start pos and cam

---
+++ Assets/1st Party/Scripts/WinScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MindHacks()
+    {
+
+    }
+
+    void Kills()
+    {
+
+    }
+
+    void Sleeps()
+    {
+
+    }
+
+    void Sneaks()
+    {
+
+    }
+}
